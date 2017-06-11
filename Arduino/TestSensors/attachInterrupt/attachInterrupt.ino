cba74452b4a2f6b43be69d8d404e8b8bb3e4b2e3
deepers14159 <jagdeepsb150@gmail.com>
2017-06-11 14:33:23 -0400

    imma push

---
+++ Arduino/TestSensors/attachInterrupt/attachInterrupt.ino
@@ -0,0 +1,41 @@
+
+#include "PololuQik.h"
+#include <SoftwareSerial.h>
+
+PololuQik2s12v10 qik(10, 9, 11);
+volatile int ticks = 0;
+void countTicks()
+{
+  
+   ticks++;  
+ 
+}
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+//  pinMode(10, INPUT);
+  qik.init();
+  attachInterrupt(0, countTicks, RISING);
+ //qik.setM0Speed(25);
+ //qik.setM1Speed(25);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+ //countTicks();
+  Serial.println(ticks);
+  
+  
+ if(ticks < 400)
+ {}
+ else
+ {
+    qik.setM0Brake(127);
+    qik.setM1Brake(127);
+}
+  
+  
+}
